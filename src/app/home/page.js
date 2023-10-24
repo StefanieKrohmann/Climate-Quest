75be57870ff9fba7183a0f0d7a51078c4f746b5f
Raj Mandloii <rajmandloi01.01@gmail.com>
2023-10-24 14:30:28 +0530

    minor changes

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -7,33 +7,37 @@ import AllQuest from "./(components)/allQuest";
 const Home = () => {
   const [activeTab, setActiveTab] = useState(1);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start sm:p-24 sm:pt-0 py-8 ">
-      <div className="sm:text-6xl text-2xl font-bold mb-12">Climate quest</div>
-      <ul className="flex flex-wrap">
-        <li className="mr-2 border-2 border-black">
-          <div
-            onClick={() => setActiveTab(1)}
-            className={`sm:py-4 py-1 sm:px-24 px-2 sm:text-2xl font-medium text-center ${
-              activeTab == 1 ? "active" : "not-active"
-            } hover:cursor-pointer`}
-          >
-            My quests
-          </div>
-        </li>
-        <li className="ml-6 border-2 border-black">
-          <div
-            onClick={() => setActiveTab(2)}
-            className={`sm:py-4 py-1 sm:px-24 px-2 sm:text-2xl font-medium text-center ${
-              activeTab == 2 ? "active" : "not-active"
-            }  hover:cursor-pointer`}
-          >
-            All quests
-          </div>
-        </li>
-      </ul>
-      {/* Pages */}
+    <main className="flex min-h-screen flex-col items-center justify-start sm:p-24 sm:pt-0 my-12">
+      <div className="fixed bg-white z-20 w-[100%] flex flex-col py-6 top-0 items-center justify-center">
+        <div className="sm:text-6xl text-2xl font-bold mb-12">
+          Climate quest
+        </div>
+        <ul className="flex flex-wrap">
+          <li className="mr-2 border-2 border-black">
+            <div
+              onClick={() => setActiveTab(1)}
+              className={`sm:py-4 py-1 sm:px-24 px-2 sm:text-2xl font-medium text-center ${
+                activeTab == 1 ? "active" : "not-active"
+              } hover:cursor-pointer`}
+            >
+              My quests
+            </div>
+          </li>
+          <li className="ml-6 border-2 border-black">
+            <div
+              onClick={() => setActiveTab(2)}
+              className={`sm:py-4 py-1 sm:px-24 px-2 sm:text-2xl font-medium text-center ${
+                activeTab == 2 ? "active" : "not-active"
+              }  hover:cursor-pointer`}
+            >
+              All quests
+            </div>
+          </li>
+        </ul>
+      </div>
 
-      {activeTab == 1 ? <MyQuest /> : <AllQuest />}
+      {/* Pages */}
+      <div className="sm:mt-44 mt-24">{activeTab == 1 ? <MyQuest /> : <AllQuest />}</div>
     </main>
   );
 };
