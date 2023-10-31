44c700ace3872ccb546d102ebbbcd89c65c438ff
Raj Mandloii <rajmandloi01.01@gmail.com>
2023-10-31 21:40:41 +0530

    minor chages in logout

--- src/app/api/users/logout/route.js
+++ src/app/api/users/logout/route.js
@@ -6,9 +6,23 @@ export async function GET() {
       message: "Logout successfull",
       success: true,
     });
-    response.cookies.delete("token")
+    
+    response.cookies.set("token", "", { httpOnly: true });
     return response;
   } catch (e) {
     return NextResponse.json({ error: e.message }, { status: 500 });
   }
 }
+
+
+// export const deleteCookie = (
+//   request: NextRequest,
+//   response: NextResponse,
+//   cookie: string
+// ) => {
+//   const { value, options } = request.cookies.getWithOptions(cookie);
+//   if (value) {
+//     response.cookies.set(cookie, value, options);
+//     response.cookies.delete(cookie);
+//   }
+// };
