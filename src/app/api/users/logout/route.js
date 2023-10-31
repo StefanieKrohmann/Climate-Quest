4d9fbf051ce3b523604a82fa6f6ec8c64ae917e2
Raj Mandloii <rajmandloi01.01@gmail.com>
2023-10-31 21:05:33 +0530

    props added to logout api

--- src/app/api/users/logout/route.js
+++ src/app/api/users/logout/route.js
@@ -6,7 +6,12 @@ export async function GET() {
       message: "Logout successfull",
       success: true,
     });
-    response.cookies.set("token", "", { httpOnly: true });
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+      secure: true, // Ensure the cookie is only sent over HTTPS
+      sameSite: "Strict", // Control cross-site request behavior
+    });
     return response;
   } catch (e) {
     return NextResponse.json({ error: e.message }, { status: 500 });
