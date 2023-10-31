7d49c13ca11e71e7931d069f8e9824e765c24626
Raj Mandloii <rajmandloi01.01@gmail.com>
2023-10-31 22:04:07 +0530

    max age added

--- src/app/api/users/logout/route.js
+++ src/app/api/users/logout/route.js
@@ -6,23 +6,15 @@ export async function GET() {
       message: "Logout successfull",
       success: true,
     });
-    
-    response.cookies.set("token", "", { httpOnly: true });
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      maxAge: 0,
+      expires: new Date(0),
+      secure: true, // Ensure the cookie is only sent over HTTPS
+      sameSite: "Strict", // Control cross-site request behavior
+    });
     return response;
   } catch (e) {
     return NextResponse.json({ error: e.message }, { status: 500 });
   }
 }
-
-
-// export const deleteCookie = (
-//   request: NextRequest,
-//   response: NextResponse,
-//   cookie: string
-// ) => {
-//   const { value, options } = request.cookies.getWithOptions(cookie);
-//   if (value) {
-//     response.cookies.set(cookie, value, options);
-//     response.cookies.delete(cookie);
-//   }
-// };
